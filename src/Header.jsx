35c3445a828d162e8a3eefb1f79db78a582c251a
Dhruv Bhatia <dbhatia03@gmail.com>
2023-12-19 12:01:50 +0530

    initial commit

---
+++ src/Header.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="bg-gray-200 py-4">
+      <div className="container mx-auto">
+        <h1 className="text-2xl font-bold text-gray-800">PDF to Braille Converter</h1>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
